4365de8587fbd853f6f7883a50e1e86296c694c9
VnPower <vnpower@disroot.org>
2023-08-28 11:20:19 +0700

    Route testing #13

--- pixivfe_test.go
+++ pixivfe_test.go
@@ -5,78 +5,6 @@ import (
 	"testing"
 )
 
-// func generate_test_client() *handler.PixivClient {
-// 	transport := &http.Transport{Proxy: http.ProxyFromEnvironment}
-// 	client := &http.Client{
-// 		Timeout:   time.Duration(5000) * time.Millisecond,
-// 		Transport: transport,
-// 	}
-
-// 	pc := &handler.PixivClient{
-// 		Client: client,
-// 		Header: make(map[string]string),
-// 		Cookie: make(map[string]string),
-// 		Lang:   "en",
-// 	}
-
-// 	pc.SetSessionID(os.Getenv("PIXIVFE_TOKEN"))
-// 	pc.SetUserAgent("Mozilla/5.0")
-
-// 	return pc
-// }
-
-// func Test_GetArtworkByID(t *testing.T) {
-// 	client := generate_test_client()
-
-// 	start := time.Now()
-// 	_, err := client.GetArtworkByID("97422582")
-// 	end := time.Since(start).Seconds()
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-
-// 	t.Logf("Took %.2f seconds", end)
-// }
-
-// func Test_GetArtworkImages(t *testing.T) {
-// 	client := generate_test_client()
-
-// 	start := time.Now()
-// 	_, err := client.GetArtworkImages("97422582")
-// 	end := time.Since(start).Seconds()
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-
-// 	t.Logf("Took %.2f seconds", end)
-// }
-
-// func Test_GetArtworkComments(t *testing.T) {
-// 	client := generate_test_client()
-
-// 	start := time.Now()
-// 	_, err := client.GetArtworkComments("97422582")
-// 	end := time.Since(start).Seconds()
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-
-// 	t.Logf("Took %.2f seconds", end)
-// }
-
-// func Test_GetRelatedArtworks(t *testing.T) {
-// 	client := generate_test_client()
-
-// 	start := time.Now()
-// 	_, err := client.GetRelatedArtworks("97422582")
-// 	end := time.Since(start).Seconds()
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-
-// 	t.Logf("Took %.2f seconds", end)
-// }
-
 func Test_Main(t *testing.T) {
 	_, err := http.Get("http://localhost:8282/")
 	if err != nil {
@@ -85,5 +13,50 @@ func Test_Main(t *testing.T) {
 	t.Logf("/ passed")
 }
 
-// func Test_GetArtworkGoroutine(t *testing.T) {
-// }
+func Test_Settings(t *testing.T) {
+	_, err := http.Get("http://localhost:8282/settings")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("Settings route passed")
+}
+
+func Test_Ranking(t *testing.T) {
+	_, err := http.Get("http://localhost:8282/ranking")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("Ranking route passed")
+}
+
+func Test_Ranking_Complex(t *testing.T) {
+	_, err := http.Get("https://pixivfe.exozy.me/ranking?content=all&mode=daily_r18&page=1&date=20230826")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("Complex ranking route passed")
+}
+
+func Test_Artwork(t *testing.T) {
+	_, err := http.Get("http://localhost:8282/artworks/111157207")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("Artwork route passed")
+}
+
+func Test_Artwork_R18(t *testing.T) {
+	_, err := http.Get("http://localhost:8282/artworks/111130033")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("R18 Artwork route passed")
+}
+
+func Test_User(t *testing.T) {
+	_, err := http.Get("http://localhost:8282/users/1960050")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("User route passed")
+}
