e7d1ca73626ff7ba41895aa37bc716aef4e994f5
vnpower <vnpower@vnpower.vnpower>
2023-10-21 15:49:26 +0700

    Fix: next page is broken when there is no page limit #34

--- handler/template.go
+++ handler/template.go
@@ -133,11 +133,12 @@ func CreatePaginator(base, ending string, current_page, max_page int) template.H
 		count++
 	}
 
-	pages += fmt.Sprintf(`<a href="%s%d%s" class="pagination-button">&rsaquo;</a>`, base, min(max_page, current_page+1), ending)
 
 	if max_page == -1 {
+		pages += fmt.Sprintf(`<a href="%s%d%s" class="pagination-button">&rsaquo;</a>`, base, current_page+1, ending)
 		pages += fmt.Sprintf(`<a href="%s%d%s" class="pagination-button" id="disabled">&raquo;</a>`, base, max_page, ending)
 	} else {
+		pages += fmt.Sprintf(`<a href="%s%d%s" class="pagination-button">&rsaquo;</a>`, base, min(max_page, current_page+1), ending)
 		pages += fmt.Sprintf(`<a href="%s%d%s" class="pagination-button">&raquo;</a>`, base, max_page, ending)
 	}
 
