15d41ec69e19be5b57eacef1c4d35a3ad1cc3410
iacore <noreply+gpg-stub@1a-insec.net>
2024-02-09 13:41:10 +0000

    (experimental) show multiple artworks on one page

---
+++ pages/artwork-multi.go
@@ -0,0 +1,48 @@
+package pages
+
+import (
+	"errors"
+	"log"
+	"strconv"
+	"strings"
+
+	core "codeberg.org/vnpower/pixivfe/v2/core/webapi"
+	"github.com/gofiber/fiber/v2"
+)
+
+func ArtworkMultiPage(c *fiber.Ctx) error {
+	ids := c.Params("ids")
+
+	artworks := []fiber.Map{}
+
+	// optimize: sequential access might be slow
+	for _, id := range strings.Split(ids, ",") {
+		if _, err := strconv.Atoi(id); err != nil {
+			return errors.New("Invalid ID.")
+		}
+	
+		illust, err := core.GetArtworkByID(c, id)
+		if err != nil {
+			return err
+		}
+	
+		metaDescription := ""
+		for _, i := range illust.Tags {
+			metaDescription += "#" + i.Name + ", "
+		}
+
+		artworks = append(artworks, fiber.Map{
+			"Illust":          illust,
+			"Title":           illust.Title,
+			"PageType":        "artwork",
+			"MetaDescription": metaDescription,
+			"MetaImage":       illust.Images[0].Original,
+		})
+	}
+	
+	log.Println("artworks:", artworks)
+
+	return c.Render("pages/artwork-multi", fiber.Map{
+		"Artworks": artworks,
+	})
+}
