50c2a7c809ca70a25253cbb5b4d615f7d7b835f4
iacore <noreply+gpg-stub@1a-insec.net>
2024-02-17 23:42:25 +0000

    fix: handle htmx error when the server is down

--- views/js/on-page-load.js
+++ views/js/on-page-load.js
@@ -1,5 +1,9 @@
 // make 4xx 5xx responses swap in as well
+addEventListener('htmx:sendError', function (event) {
+  document.write("A network error prevented an HTTP request from occurring, which is likely because the server is down. Check your Developer Console's Network tab to know what exactly is wrong.")
+});
 addEventListener('htmx:beforeOnLoad', function (event) {
+  // console.log("%o", event)
   event.detail.shouldSwap = true;
   event.detail.isError = false;
 });
