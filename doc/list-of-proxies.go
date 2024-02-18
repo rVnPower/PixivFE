5e9c637cfadf5540625214d5ee97b951d16ea673
VnPower <vnpower@disroot.org>
2024-02-18 09:37:22 +0700

    Update image proxies

--- doc/list-of-proxies.go
+++ doc/list-of-proxies.go
@@ -2,11 +2,15 @@ package doc
 
 // the list of proxies on /settings
 var BuiltinProxyList = []string{
+	// WE ARE NOT AFFILIATED WITH MOST OF THE PROXIES!!!!
+	// !!!!!!!
 	"https://pximg.exozy.me",
 	"https://pixiv.ducks.party",
-	"https://***.nyan.xyz",
-	"https://pixiv.tatakai.top",
-	"https://i.****.best",
-	"https://img.chaotic.ninja",
-	"https://pixiv.cat",
+	"https://pximg.cocomi.eu.org",
+	"https://sex.nyan.xyz",
+	"https://i.loli.best",
+	"https://mima.localghost.org/proxy/pximg",
+	"https://i.pixiv.re",
+	// "https://pixiv.tatakai.top", // dead due to us :(
+	// "https://pximg.chaotic.ninja", // incompatible
 }
